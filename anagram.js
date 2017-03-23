0c36674e62373e07b441370262219cdbd4de445c
Chick202 <Chick202@users.noreply.github.com>
2017-03-23 10:32:46 -0600

    Update anagram.js
    The correct implementation using associative lists.

--- anagram.js
+++ anagram.js
@@ -1,62 +1,30 @@
 //Given a list of words, arrange anagrams as groups
 function anagram(li){
-	
-	var key = "";
-    var newArr = new Array();
-	
-	for(var i=0; i<li.length; i++){
-		
-		key = li[i].split('').sort().join('');
-		newArr[key] = li[i];
-	}
   
-    var keys = [];
-    for(var key in newArr){
-      if(newArr.hasOwnProperty(key))
-      {
-        keys.push(key);
-      }
-    }
+  var key = '';
+  var list = {};
   
-    newArr.sort();
-  
-    for(key in newArr){
-      //console.log(newArr[key]);
-      console.log(key);
-    }
-	
-}
-
-
-var li = ["AMOR", "ARMO", "ESPONJA", "JAMON", "JAPONES", "MARO", "MONJA", "MOJAN", "MORA", "OMAR", "RAMO", "ROMA"];
-anagram(li);
-
-
-//Given a list of words, arrange anagrams as groups
-function anagram(li){
-	
-	var key = "";
-    var newArr = new Array();
-    var keys = [];
-	
-	for(var i=0; i<li.length; i++){
-		
-		key = li[i].split('').sort().join('');
-		//newArr[key] = li[i];
-        newArr[li[i]] = key;
-        keys[i] = li[i];
-	}
-   
-    keys.sort();
+  for(i=0;i<li.length;i++){
     
-  
-    for(var j=0; j<keys.length;j++){
-      var val = keys[j];
-      console.log(val);
-      //console.log(newArr[val]);
+    key = li[i].split('').sort().join('');
+    
+    if(key in list){
+      list[key].push(li[i]);
     }
-	
+    
+    else{
+      list[key] = [li[i]];
+    }
+   
+  }
+  
+  for(key in list){
+    console.log(list[key]);  
+  }
+  
+  
 }
+
 
 
 var li = ["AMOR", "ARMO", "ESPONJA", "JAMON", "JAPONES", "MARO", "MONJA", "MOJAN", "MORA", "OMAR", "RAMO", "ROMA"];
